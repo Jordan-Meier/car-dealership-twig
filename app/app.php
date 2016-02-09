2ec3bc84c3c166e4db9c07d029cae80074a9aaa9
Mary Warrington and Ryan Brown <marywarrington@gmail.com>
2016-02-09 10:24:22 -0800

    Add form to limit results

--- app/app.php
+++ app/app.php
@@ -4,7 +4,35 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
+    $app->get("/car_form", function() {
+        return "
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+                <title>Find your car!</title>
+            </head>
+            <body>
+                <div class='container'>
+                    <h1>Find Your Car!</h1>
+                    <form action='/car_results'>
+                        <div class='form-group'>
+                          <label for='price'>Enter Maximum Price:</label>
+                          <input id='price' name='price' class='form-control' type='number'>
+                        </div>
+                        <div class='form-group'>
+                          <label for='mileage'>Enter Maximum Mileage:</label>
+                          <input id='mileage' name='mileage' class='form-control' type='number'>
+                        </div>
+                        <button type='submit' class='btn-success'>Create</button>
+                    </form>
+                </div>
+            </body>
+        </html>
+        ";
+    });
+
+    $app->get("/car_results", function() {
         $honda = new Car("1999 Honda CRV", 90000, 6000, "img/honda.jpg");
         $tesla = new Car("2014 Tesla Model S", 5000, 35000, "img/tesla.jpg");
         $nissan = new Car("2013 Nissan Leaf", 8000, 20000, "img/leaf.jpg");
@@ -12,8 +40,15 @@
         $toyota->setPrice("$12000");
         $cars = array($honda, $tesla, $nissan, $toyota);
 
-        $output = "";
+        $cars_matching_search = array();
         foreach ($cars as $car) {
+            if ($car->worthBuying(($_GET["price"]), ($_GET["mileage"]))) {
+                array_push($cars_matching_search, $car);
+            }
+        }
+
+        $output = "";
+        foreach ($cars_matching_search as $car) {
             $output = $output . "<div class='row'>
                 <div class='col-md-6'>
                     <h1>" . $car->getModel() . "</h1>
@@ -27,7 +62,6 @@
             ";
         }
         return $output;
-
     });
 
     return $app;
