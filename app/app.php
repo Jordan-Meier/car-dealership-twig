ee7c92afe2b7ed17b443865c71d05126d2724ef3
Jordan Meier and Mary Warrington <marywarrington@gmail.com>
2016-02-11 09:10:55 -0800

    move hard coded cars to session array, clean up car results route and remove all html from app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,15 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/dealership.php";
+    require_once __DIR__."/../src/Car.php";
 
     session_start();
     if (empty($_SESSION['list_of_cars'])) {
-        $_SESSION['list_of_cars'] = array();
+        $_SESSION['list_of_cars'] = array(
+            new Car("1999 Honda CRV", 90000, 6000, "img/honda.jpg"),
+            new Car("2014 Tesla Model S", 5000, 35000, "img/tesla.jpg"),
+            new Car("2013 Nissan Leaf", 8000, 20000, "img/leaf.jpg"),
+            new Car("2009 Toyota Prius", 20000, 15000, "img/toyota.jpg"),
+        );
     }
 
     $app = new Silex\Application();
@@ -14,19 +19,15 @@
 
     //routes
 
-    $app->get("/car_form", function() {
+    $app->get("/car_form", function() use ($app){
         return $app['twig']->render('search.html.twig')
         ;
     });
 
-    $app->get("/car_results", function() {
-        $honda = new Car("1999 Honda CRV", 90000, 6000, "img/honda.jpg");
-        $tesla = new Car("2014 Tesla Model S", 5000, 35000, "img/tesla.jpg");
-        $nissan = new Car("2013 Nissan Leaf", 8000, 20000, "img/leaf.jpg");
-        $toyota = new Car("2009 Toyota Prius", 20000, 15000, "img/toyota.jpg");
-        $toyota->setPrice("$12000");
-        $cars = array($honda, $tesla, $nissan, $toyota);
+    $app->get("/car_results", function() use ($app){
 
+
+        $cars = Car::getAll();
         $cars_matching_search = array();
         foreach ($cars as $car) {
             if ($car->worthBuying(($_GET["price"]), ($_GET["mileage"]))) {
@@ -34,21 +35,7 @@
             }
         }
 
-        $output = "";
-        foreach ($cars_matching_search as $car) {
-            $output = $output . "<div class='row'>
-                <div class='col-md-6'>
-                    <h1>" . $car->getModel() . "</h1>
-                </div>
-                <div class='col-md-6'>
-                    <p>$" . $car->getPrice() . "</p>
-                    <p>" . $car->getMiles() . "</p>
-                    <img src=" . $car->getPhoto() . ">
-                </div>
-            </div>
-            ";
-        }
-        return $output;
+
     });
 
     return $app;
